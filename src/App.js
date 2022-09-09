a5572220a78e09f53dbf7df3a6e0bf31b4fb51bc
Nachwera Richard <nachwerarichard@gmail.com>
2022-09-09 11:07:18 +0300

    reactjs app myapp1
    Signed-off-by: Nachwera Richard <nachwerarichard@gmail.com>

--- src/App.js
+++ src/App.js
@@ -6,8 +6,9 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Richard <code>src/App.js</code> nachwerarichard@gmail.com.
         </p>
         <a
           className="App-link"
